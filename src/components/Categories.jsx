944f587dd6aa812094243fa0de9a2249a8977d63
krautz01 <bekturnurlanov01@gmail.com>
2023-02-01 17:27:40 +0600

    adding sort by category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,42 +1,28 @@
-import React, { useState } from 'react'
+import React from "react";
 
-export default function Categories() {
-  const [activeCatigoties, setActiveCatigoties] = useState(0)
-  
+export default function Categories({ value, onClickCategory }) {
   const categories = [
-    {
-      title: 'Все',
-      id: 0
-    },
-    {
-      title: 'Мясные',
-      id: 1
-    },
-    {
-      title: 'Вегетарианские',
-      id: 2
-    },
-    {
-      title: 'Гриль',
-      id: 3
-    },
-    {
-      title: 'Острые',
-      id: 4
-    },
-    {
-      title: 'Закрытые',
-      id: 5
-    }
+    "Все",
+    "Мясные",
+    "Вегетарианские",
+    "Гриль",
+    "Острые",
+    "Закрытые",
   ];
 
   return (
     <div className="categories">
       <ul>
-      {categories.map((category) => (
-        <li key={category.id} onClick={() => setActiveCatigoties(category.id)} className={activeCatigoties == category.id ? 'active' : ''}>{category.title}</li>
-      ))}
+        {categories.map((categoryName, i) => (
+          <li
+            key={i}
+            onClick={() => onClickCategory(i)}
+            className={value == i ? "active" : ""}
+          >
+            {categoryName}
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,12 @@ import PizzaBlockSkeleton from "../components/PizzaBlockSkeleton";
 export default function Home() {
   const [pizzas, setPizzas] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryID, setCategoryID] = React.useState(0);
+  const [sortType, setSortType] = React.useState(0);
 
   React.useEffect(() => {
-    fetch("https://63d3ab7d8d4e68c14eafb6bd.mockapi.io/pizzas")
+    setIsLoading(true);
+    fetch('https://63d3ab7d8d4e68c14eafb6bd.mockapi.io/pizzas?category=' + categoryID)
       .then((res) => {
         return res.json();
       })
@@ -17,13 +20,16 @@ export default function Home() {
         setPizzas(arr);
         setIsLoading(false);
       });
-      window.scrollTo(0,0)
-  }, []);
+    window.scrollTo(0, 0);
+  }, [categoryID]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
+        <Categories
+          value={categoryID}
+          onClickCategory={(i) => setCategoryID(i)}
+        />
         <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
